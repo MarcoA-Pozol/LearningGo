f57f47700f2fe28f2d31a451339f32e76e7e2ca4
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-06 13:56:23 -0600

    Fist functions with Go

--- first_steps.go
+++ first_steps.go
@@ -1,8 +1,33 @@
+// Main method with first package "fmt"
 package main
+
 import "fmt" //Formating Package
 
-func main(){
+func main() {
 	fmt.Println("This is Go!")
 	fmt.Println("And go is really fast!")
 	fmt.Println("You cannot stop it!")
+
+	// Variables
+	var name string = "Marco Antonio"
+	last_name := "Pozol" // Var type autodetection
+	const PI = 3.14
+
+	fmt.Println(name)
+	fmt.Println(last_name)
+	fmt.Println(PI)
+
+	fmt.Println(sumNumbers())
+	fmt.Println(getTwoNumbersAverage(458, 540))
+}
+
+func sumNumbers() int {
+	var numberone = 500
+	var numbertwo = 600
+	var result = numberone + numbertwo
+	return result
+}
+
+func getTwoNumbersAverage(a int, b int) int {
+	return (a + b) / 2
 }
