b2a3f65ec51637fd1f17e72ae0ca1a3fca54d156
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-04-13 22:18:07 -0600

    Example of Structs and interfaces with methods with Vehicles struct.

--- interfaces_and_polymorphism.go
+++ interfaces_and_polymorphism.go
@@ -13,6 +13,23 @@ func main() {
 
 	fmt.Println(person_one.AngryGreet(person_three))
 	fmt.Println(person_three.HappyGreet(person_two))
+	
+	// Defining Vehicle 
+	vehicle_one := Vehicle{Name:"Ford Raptor 2025", Horsepower:690, VType:"Ground", Color:"Orange"}
+	vehicle_two := Vehicle{Name:"Boing B-97 AA", Horsepower:690, VType:"Air", Color:"White-Gray"}
+	vehicle_three := Vehicle{Name:"Dgiant Vessel G200", Horsepower:850, VType:"Sea", Color:"Blue-White"}
+	
+	fmt.Println(vehicle_one.StartEngine())
+	fmt.Println(vehicle_two.StartEngine())
+	fmt.Println(vehicle_three.StartEngine())
+	
+	fmt.Println(vehicle_one.MoveTo(12500, "Minessota"))
+	fmt.Println(vehicle_two.MoveTo(135000, "Houston, Texas"))
+	fmt.Println(vehicle_three.MoveTo(96600, "Pacific Ocean"))
+	
+	fmt.Println(vehicle_one.StopEngine())
+	fmt.Println(vehicle_one.StopEngine())
+	fmt.Println(vehicle_one.StopEngine())
 }
 
 // Person struct
@@ -40,4 +57,35 @@ func (p Person) AngryGreet(r Person) string {
 	randomIndex := rand.Intn(len(angry_greetings))
 	greet := angry_greetings[randomIndex]
 	return greet
+}
+
+
+// Vehicle struct
+type Vehicle struct {
+	Name string
+	Horsepower int
+	VType string
+	Color string
+}
+
+// Interface for "Vehicle"
+type Vehicle interface {
+	StartEngine(Vehicle) string
+	MoveTo(Vehicle, direction string, meters int) string
+	StopEngine(Vehicle) string
+}
+
+func (v Vehicle) StartEngine() string {
+	result = fmt.Sprintf("%s has started its engine.", v.Name)
+	return result
+}
+
+func (v Vehicle) MoveTo(direction string, meters string) string {
+	result = fmt.Sprintf("This vehicle '%s' has moved %s meters to %s.", v.Name, meters, direction)
+	return result
+}
+
+func (v Vehicle) StopEngine() string {
+	result = fmt.Sprintf("%s has stop its engine.", v.Name)
+	return result
 }
